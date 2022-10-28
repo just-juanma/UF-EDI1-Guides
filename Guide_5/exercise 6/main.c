173375094816e733d359286b6f0400d77fd44de5
Juan Manuel <jugoka947@gmail.com>
2022-10-27 23:25:53 -0300

    update

--- Guide_5/exercise 6/main.c
+++ Guide_5/exercise 6/main.c
@@ -16,19 +16,20 @@ typedef struct {
 
 } dataQueue_t;
 
-dataQueue_t* generateQueue(size_t* n);
+void generateQueue(dataQueue_t** FIFO, size_t* n);
 void queue(int8_t node, dataQueue_t* FIFO, size_t* n);
 void dequeue(dataQueue_t* FIFO, size_t* n);
 int8_t getBeg(dataQueue_t* FIFO);
 int8_t getEnd(dataQueue_t* FIFO);
-
+void resize(dataQueue_t** FIFO, size_t n);
+ 
 int main(int argc, char* argv[]) 
 {
    srand(time(NULL));
    dataQueue_t* FIFO = NULL;
    int8_t data1 = rand() % INT8_MAX, data2 = rand() % INT8_MAX, data3 = rand() % INT8_MAX;
    size_t n = 0;
-   FIFO = generateQueue(&n);
+   generateQueue(&FIFO, &n);
    
    queue(data1, FIFO, &n);
 
@@ -47,11 +48,10 @@ int main(int argc, char* argv[])
 /// @brief queue generation
 /// @param n size of FIFO
 /// @return FIFO pointer with dinamic array allocation
-dataQueue_t* generateQueue(size_t* n)
+void generateQueue(dataQueue_t** FIFO, size_t* n)
 {
-    dataQueue_t* FIFO = calloc(1, sizeof(dataQueue_t));
-    (*FIFO).queue = calloc(++(*n), sizeof(int8_t));
-    return FIFO;
+    *FIFO = calloc(1, sizeof(dataQueue_t));
+    (*(*FIFO)).queue = calloc(++(*n), sizeof(int8_t));
 }
 
 /// @brief add a node at the end of queue
@@ -64,7 +64,7 @@ void queue(int8_t node, dataQueue_t* FIFO, size_t* n)
    if(*n == 1)
        (*FIFO).beg = node;
    (*FIFO).end = node;
-   (*FIFO).queue = realloc((*FIFO).queue, ++(*n));
+   resize(&FIFO, ++*n);
 }
 
 /// @brief erase the first value of the queue
@@ -79,9 +79,16 @@ void dequeue(dataQueue_t* FIFO, size_t* n)
     (*FIFO).beg = *((*FIFO).queue);
     if(*n == 1)
          (*FIFO).end = *((*FIFO).queue);
-    *n--;
+    resize(&FIFO, --*n);
 }
 
+/// @brief modifies array length by n
+/// @param FIFO queue
+/// @param n new size
+void resize(dataQueue_t** FIFO, size_t n)
+{
+    (*(*FIFO)).queue = realloc((*(*FIFO)).queue, n);
+}
 
 /// @return beginning of queue
 int8_t getBeg(dataQueue_t* FIFO)
